a7949074d16605296a7c5c907f9d0567cbc51aab
czy <1540982333@qq.com>
2019-07-29 07:25:08 +0800

    未知改动

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    class="player"
-    v-show='playlist.length > 0'
-  >
+  <div class="player" v-show="playlist.length > 0">
     <transition
       name="normal"
       @enter="enter"
@@ -10,84 +7,46 @@
       @leave="leave"
       @after-leave="afterLeave"
     >
-      <div
-        class="normal-player"
-        v-show="fullScreen"
-      >
+      <div class="normal-player" v-show="fullScreen">
         <div class="background">
-          <img
-            width="100%"
-            height="100%"
-          >
+          <img width="100%" height="100%" />
         </div>
         <div class="top">
-          <div
-            class="back"
-            @click="back"
-          >
+          <div class="back" @click="back">
             <i class="icon-back"></i>
           </div>
-          <h1
-            class="title"
-            v-html="currentSong.name"
-          ></h1>
-          <h2
-            class="subtitle"
-            v-html="currentSong.singer"
-          ></h2>
+          <h1 class="title" v-html="currentSong.name"></h1>
+          <h2 class="subtitle" v-html="currentSong.singer"></h2>
         </div>
         <div class="middle">
           <div class="middle-l">
-            <div
-              class="cd-wrapper"
-              ref="cdWrapper"
-            >
+            <div class="cd-wrapper" ref="cdWrapper">
               <div class="cd">
-                <img
-                  :src="currentSong.image"
-                  class="image"
-                  :class="cdCls"
-                >
+                <img :src="currentSong.image" class="image" :class="cdCls" />
               </div>
             </div>
           </div>
         </div>
         <div class="bottom">
           <div class="progress-wrapper">
-            <span class="time time-l">{{format(currentTime)}}</span>
-            <div class="progress-bar-wrapper"></div>
-            <span class="time time-r">{{format(currentSong.duration)}}</span>
+            <span class="time time-l">{{ format(currentTime) }}</span>
+            <div class="progress-bar-wrapper">
+              <progress-bar></progress-bar>
+            </div>
+            <span class="time time-r">{{ format(currentSong.duration) }}</span>
           </div>
           <div class="operators">
             <div class="icon i-left">
               <i class="icon-sequence"></i>
             </div>
-            <div
-              class="icon i-left"
-              :class="disableCls"
-            >
-              <i
-                @click="prev"
-                class="icon-prev"
-              ></i>
+            <div class="icon i-left" :class="disableCls">
+              <i @click="prev" class="icon-prev"></i>
             </div>
-            <div
-              class="icon i-center"
-              :class="disableCls"
-            >
-              <i
-                @click="togglePlaying"
-                :class="playIcon"
-              ></i>
+            <div class="icon i-center" :class="disableCls">
+              <i @click="togglePlaying" :class="playIcon"></i>
             </div>
-            <div
-              class="icon i-right"
-              :class="disableCls"
-            >
-              <i
-                @click="next"
-                class="icon-next"
-              ></i>
+            <div class="icon i-right" :class="disableCls">
+              <i @click="next" class="icon-next"></i>
             </div>
             <div class="icon i-right">
               <i class="icon icon-not-favorite"></i>
@@ -97,11 +56,7 @@
       </div>
     </transition>
     <transition name="mini">
-      <div
-        class="mini-player"
-        v-show="!fullScreen"
-        @click="open"
-      >
+      <div class="mini-player" v-show="!fullScreen" @click="open">
         <div class="icon">
           <div class="imgWrapper">
             <img
@@ -109,24 +64,15 @@
               :class="cdCls"
               width="40"
               height="40"
-            >
+            />
           </div>
         </div>
         <div class="text">
-          <h2
-            class="name"
-            v-html="currentSong.name"
-          ></h2>
-          <p
-            class="desc"
-            v-html="currentSong.singer"
-          ></p>
+          <h2 class="name" v-html="currentSong.name"></h2>
+          <p class="desc" v-html="currentSong.singer"></p>
         </div>
         <div class="control">
-          <i
-            @click.stop="togglePlaying"
-            :class="miniIcon"
-          ></i>
+          <i @click.stop="togglePlaying" :class="miniIcon"></i>
         </div>
         <div class="control">
           <i class="icon-playlist"></i>
@@ -147,6 +93,7 @@
 import { mapGetters, mapMutations } from 'vuex'
 import animations from 'create-keyframe-animation'
 import { prefixStyle } from 'common/js/dom'
+import ProgressBar from 'base/progress-bar/progress-bar'
 
 const transform = prefixStyle('transform')
 
@@ -316,6 +263,9 @@ export default {
         newPlaying ? audio.play() : audio.pause()
       })
     }
+  },
+  components: {
+    ProgressBar
   }
 }
 </script>
