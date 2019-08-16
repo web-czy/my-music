60e25df565b6d076ebecbcc2899cf7bec98c9f86
study-demo <1540982333@qq.com>
2019-08-16 17:56:52 +0800

    路由懒加载，移动调试工具

--- src/main.js
+++ src/main.js
@@ -1,23 +1,29 @@
-import 'babel-polyfill'
-import Vue from 'vue'
-import App from './App.vue'
-import fastclick from 'fastclick'
-import router from './router/index'
-import store from './store'
-import VueLazyLoad from 'vue-lazyload'
+import 'babel-polyfill';
+import Vue from 'vue';
+import App from './App.vue';
+import fastclick from 'fastclick';
+import router from './router/index';
+import store from './store';
+import VueLazyLoad from 'vue-lazyload';
 
-import './common/stylus/index.styl'
+import './common/stylus/index.styl';
 
-Vue.config.productionTip = false
+/* eslint-disable no-unused-vars */
+// import VConsole from 'vconsole';
+// var vConsole = new VConsole();
+
+// console.log('text');
+
+Vue.config.productionTip = false;
 // 让document.body下的所有div及其他元素的点击都没有300毫秒的延迟
-fastclick.attach(document.body)
+fastclick.attach(document.body);
 
 Vue.use(VueLazyLoad, {
   loading: require('common/image/default.png')
-})
+});
 
 new Vue({
   render: h => h(App),
   router,
   store
-}).$mount('#app')
+}).$mount('#app');
